0f5ccb7486a0d8e2c48fe33754d23c2cbf4b3bc0
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-05-20 13:54:08 -0700

    Add SQL code, update support library version

---
+++ app/src/main/java/team3128/org/attendancescanner/database/AttendanceDatabase.java
@@ -0,0 +1,106 @@
+package team3128.org.attendancescanner.database;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+/**
+ * Created by Jamie on 5/20/2015.
+ */
+public class AttendanceDatabase
+{
+	AttendanceOpenHelper helper;
+
+	public void addScan(int studentID)
+	{
+		if(!studentExists(studentID))
+		{
+			Log.d("AttendanceDatabase", "Auto-adding student with ID " + studentID);
+			addStudent(studentID, null);
+		}
+
+		// Gets the data repository in write mode
+		SQLiteDatabase db = helper.getWritableDatabase();
+
+		// Create a new map of values, where column names are the keys
+		ContentValues values = new ContentValues();
+		values.put(Tables.ScanTimes.COLUMN_NAME_STUDENT_ID, studentID);
+		values.put(Tables.ScanTimes.COLUMN_NAME_TIME, System.currentTimeMillis());
+
+		db.insert(Tables.ScanTimes.TABLE_NAME, null, values);
+	}
+
+	public boolean studentExists(int studentID)
+	{
+		SQLiteDatabase db = helper.getReadableDatabase();
+
+		
+		Cursor cursor = db.query(Tables.Students.TABLE_NAME,
+						new String[]{Tables.Students.COLUMN_NAME_STUDENT_ID},
+						Tables.Students.COLUMN_NAME_STUDENT_ID + " = ?",
+						new String[]{Integer.toString(studentID)},
+						null, null, null);
+
+		return cursor.getCount() > 0;
+	}
+
+	/**
+	 * Add a student to the database.  Assumes that the student doesn't already exist.
+	 * @param studentID
+	 * @param name
+	 */
+	public void addStudent(int studentID, String name)
+	{
+		// Gets the data repository in write mode
+		SQLiteDatabase db = helper.getWritableDatabase();
+
+		// Create a new map of values, where column names are the keys
+		ContentValues values = new ContentValues();
+		values.put(Tables.Students.COLUMN_NAME_STUDENT_ID, studentID);
+		values.put(Tables.Students.COLUMN_NAME_STUDENT_NAME, name);
+
+		db.insert(Tables.Students.TABLE_NAME, null, values);
+	}
+
+	/**
+	 * Change the student with the given ID's name to the given one.
+	 * @param studentID
+	 * @param name
+	 */
+	public void updateStudent(int studentID, String name)
+	{
+
+		// Gets the data repository in write mode
+		SQLiteDatabase db = helper.getWritableDatabase();
+
+		// Create a new map of values, where column names are the keys
+		ContentValues values = new ContentValues();
+		values.put(Tables.Students.COLUMN_NAME_STUDENT_NAME, name);
+
+		db.update(Tables.Students.TABLE_NAME,
+						values,
+						Tables.Students.COLUMN_NAME_STUDENT_ID + " = ?",
+						new String[]{Integer.toString(studentID)});
+	}
+
+	/**
+	 * Remove the student with the given ID.
+	 *
+	 * This also removes any scan data associated with this student.
+	 * @param studentID
+	 */
+	public void removeStudent(int studentID)
+	{
+		SQLiteDatabase db = helper.getWritableDatabase();
+
+		//remove associated scans
+		db.delete(Tables.ScanTimes.TABLE_NAME,
+						Tables.ScanTimes.COLUMN_NAME_STUDENT_ID + " = ?",
+						new String[]{Integer.toString(studentID)});
+
+		//remove the actual student
+		db.delete(Tables.Students.TABLE_NAME,
+						Tables.Students.COLUMN_NAME_STUDENT_ID + " = ?",
+						new String[]{Integer.toString(studentID)});
+	}
+}
---
+++ app/src/main/java/team3128/org/attendancescanner/database/Tables.java
@@ -0,0 +1,49 @@
+package team3128.org.attendancescanner.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * This class defines constants for the row names of the tables.
+ *
+ * Note: some of this code taken from the example at http://developer.android.com/training/basics/data-storage/databases.html
+ */
+public class Tables
+{
+	private Tables() {}
+
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String COMMA_SEP = ",";
+
+	/* Inner class that defines the table contents */
+	public static abstract class ScanTimes implements BaseColumns
+	{
+		public static final String TABLE_NAME = "scanTimes";
+
+		//use SQLite's built-in rowid
+		public static final String _ID = "rowid";
+
+		public static final String COLUMN_NAME_STUDENT_ID = "studentID";
+		public static final String COLUMN_NAME_TIME = "scanTime";
+
+		public static final String SQL_CREATE_TABLE =
+						"CREATE TABLE " + TABLE_NAME + " (" +
+										COLUMN_NAME_STUDENT_ID + " INTEGER" + COMMA_SEP +
+										"FOREIGN KEY(" + COLUMN_NAME_STUDENT_ID + " REFERENCES " + Students.TABLE_NAME + "(" + Students.COLUMN_NAME_STUDENT_ID + ")" + COMMA_SEP +
+										COLUMN_NAME_TIME + " INTEGER" +  //stored as seconds since start of UNIX epoch
+						" )";
+	}
+
+	/* Inner class that defines the table contents */
+	public static abstract class Students implements BaseColumns
+	{
+		public static final String TABLE_NAME = "students";
+		public static final String COLUMN_NAME_STUDENT_ID = "studentID";
+		public static final String COLUMN_NAME_STUDENT_NAME = "scanDay";
+
+		public static final String SQL_CREATE_TABLE =
+						"CREATE TABLE " + TABLE_NAME + " (" +
+										COLUMN_NAME_STUDENT_ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+										COLUMN_NAME_STUDENT_NAME + TEXT_TYPE +
+										" )";
+	}
+}
